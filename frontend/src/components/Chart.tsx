c612ca72acdc7cb72cd0a270c93389712bbbbef0
yossiyoo <y.syota731@icloud.com>
2025-03-25 09:56:59 +0900

    Create Chart.tsx

---
+++ frontend/src/components/Chart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chart: React.FC = () => {
+  return (
+    <div>
+      <h2>Upward Trending Chart</h2>
+      <img src="https://www.example.com/upward-trending-chart.png" alt="Upward Trending Chart" />
+    </div>
+  );
+};
+
+export default Chart;
