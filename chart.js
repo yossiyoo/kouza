ba74088878a83173fdb4a4354f7aeb54b06d2b45
yossiyoo <y.syota731@icloud.com>
2025-03-25 11:37:53 +0900

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,15 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const generateRandomData = (numPoints, startValue, endValue) => {
+    const generateUptrendData = (numPoints, startValue, endValue) => {
         let data = [];
-        let value = startValue;
+        let step = (endValue - startValue) / (numPoints - 1);
         
-        for (let i = 0; i < numPoints - 1; i++) {
-            const change = (Math.random() - 0.5) * 2000; // -1000から+1000のランダムな変動
-            value += change;
-            data.push(value);
+        for (let i = 0; i < numPoints; i++) {
+            data.push(startValue + step * i);
         }
         
-        data.push(endValue); // 最終値を設定
         return data;
     };
 
@@ -19,8 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
         data: {
             labels: Array.from({length: 30}, (_, i) => `time ${i + 1}`), // 30日分のラベル
             datasets: [{
-                label: 'ランダム株価データ',
-                data: generateRandomData(30, 100000, 135000), // 初期値100000、最終値135000
+                label: '右肩上がりの株価データ',
+                data: generateUptrendData(30, 100000, 135000), // 初期値100000、最終値135000
                 borderColor: 'rgba(75, 192, 192, 1)',
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 fill: false,
